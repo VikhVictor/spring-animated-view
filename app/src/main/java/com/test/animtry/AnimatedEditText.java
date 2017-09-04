f68d43fb5a46a4f4ee26a94c6be156bcdf169c91
victor <victor.vikhrov.a@gmail.com>
2017-09-04 23:42:37 +0300

    keyboard behavior

--- app/src/main/java/com/test/animtry/AnimatedEditText.java
+++ app/src/main/java/com/test/animtry/AnimatedEditText.java
@@ -1,14 +1,21 @@
 package com.test.animtry;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.graphics.Rect;
+import android.support.animation.DynamicAnimation;
 import android.support.animation.SpringAnimation;
 import android.support.v4.content.ContextCompat;
+import android.text.InputType;
 import android.util.AttributeSet;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -17,7 +24,7 @@ import android.widget.TextView;
  * Created by victor on 31.08.17.
  */
 
-public class AnimatedEditText extends RelativeLayout implements View.OnClickListener, View.OnTouchListener {
+public class AnimatedEditText extends RelativeLayout implements View.OnClickListener, View.OnTouchListener, TextView.OnEditorActionListener, View.OnFocusChangeListener, DynamicAnimation.OnAnimationEndListener {
 
     private static final int STATE_CLOSED = 0;
     private static final int STATE_OPENED = 1;
@@ -83,9 +90,14 @@ public class AnimatedEditText extends RelativeLayout implements View.OnClickList
         //input.setFocusable(false);
         //input.setClickable(false);
         //input.setEnabled(false);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        input.setLines(1);
+        input.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        input.setOnEditorActionListener(this);
         input.setOnTouchListener(this);
         input.setLayoutParams(iParams);
         input.setText("Some text");
+        input.setOnFocusChangeListener(this);
 
         input.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
@@ -93,8 +105,11 @@ public class AnimatedEditText extends RelativeLayout implements View.OnClickList
                 input.getViewTreeObserver().removeOnGlobalLayoutListener(this);
                 animToLeft = SpringAnimationUtils.createSpringAnimation(input, SpringAnimationUtils.TRANSLATION_X,
                         12, 10);
+                animToLeft.addEndListener(AnimatedEditText.this);
+
                 animToRight = SpringAnimationUtils.createSpringAnimation(input, SpringAnimationUtils.TRANSLATION_X,
                         input.getX(), 30);
+                animToRight.addEndListener(AnimatedEditText.this);
             }
         });
 
@@ -104,16 +119,31 @@ public class AnimatedEditText extends RelativeLayout implements View.OnClickList
     @Override
     public void onClick(View v) {
         if (state == STATE_CLOSED) {
-            animToHide.start();
-            animToLeft.start();
-            input.requestFocus();
-            state = STATE_OPENED;
-        } else if (state == STATE_OPENED) {
-            animToVisible.start();
-            animToRight.start();
-            state = STATE_CLOSED;
+            openInput();
+            showKeyboard();
         }
+    }
 
+    private void closeInput() {
+        animToVisible.start();
+        animToRight.start();
+        hideKeyboard();
+    }
+
+    private void openInput() {
+        animToHide.start();
+        animToLeft.start();
+        input.requestFocus();
+    }
+
+    private void hideKeyboard() {
+        InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(input.getWindowToken(), 0);
+    }
+
+    private void showKeyboard() {
+        InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(input, InputMethodManager.SHOW_IMPLICIT);
     }
 
     @Override
@@ -124,5 +154,33 @@ public class AnimatedEditText extends RelativeLayout implements View.OnClickList
         }
 
         return false;
+    }
+
+
+
+    @Override
+    public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
+            input.clearFocus();
+            closeInput();
+        }
+        return false;
+    }
+
+
+    @Override
+    public void onFocusChange(View view, boolean hasFocus) {
+        if (!hasFocus) {
+            closeInput();
+        }
+    }
+
+    @Override
+    public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+        if (state == STATE_CLOSED) {
+            state = STATE_OPENED;
+        } else if (state == STATE_OPENED) {
+            state = STATE_CLOSED;
+        }
     }
 }
--- app/src/main/java/com/test/animtry/MainActivity.java
+++ app/src/main/java/com/test/animtry/MainActivity.java
@@ -2,10 +2,12 @@ package com.test.animtry;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Rect;
 import android.support.animation.FloatPropertyCompat;
 import android.support.animation.SpringAnimation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.view.inputmethod.InputMethodManager;
@@ -35,6 +37,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if ( v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    v.clearFocus();
+                }
+            }
+        }
+        return super.dispatchTouchEvent( event );
+    }
 
     public static void hideSoftKeyboard (Activity activity, View view)
     {
